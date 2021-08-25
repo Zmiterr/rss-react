ee0eae1226aa232c58061b7683a00452e574857e
Zmiter <ozmiter@gmail.com>
2021-08-25 23:31:22 +0300

    feat: create work template

---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react';
+
+
+import './App.scss';
+
+export default class App extends React.Component {
+	render = (): ReactElement => {
+		return (
+			<div className="App">
+			</div>
+		);
+	};
+}
