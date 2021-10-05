d8f5f9e8b314c7525ef579144612259df131bb2a
Zmiter <ozmiter@gmail.com>
2021-10-05 18:30:25 +0300

    feat: install and apply redux-thunk

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,8 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import pageReducer from './pageReducer';
 import articlesReducer from './articlesReducer';
+import thunk from 'redux-thunk';
 
 export type RootState = ReturnType<typeof rootReducer>;
 
@@ -10,6 +11,6 @@ const rootReducer = combineReducers({
 	articles: articlesReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
